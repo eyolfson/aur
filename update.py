f698e8a802ae18ec607e928a44267a62ccf8160f
Jon Eyolfson <jon@eyl.io>
2016-07-26 05:43:22 -0400

    emacs-theme-solarized-git renamed to emacs-color-theme-solarized r373-1

--- update.py
+++ update.py
@@ -20,7 +20,7 @@ from subprocess import call
 
 skipped = {'sabnzbd', 'python2-pygments-style-solarized', 'eyl-launcher', 'firefox-ublock-origin'}
 
-vcs_package = {'sickbeard', 'oh-my-zsh', 'emacs-rust-mode', 'packer', 'firefox-pentadactyl'}
+vcs_package = {'sickbeard', 'oh-my-zsh', 'emacs-rust-mode', 'packer', 'firefox-pentadactyl', 'emacs-color-theme-solarized'}
 
 for arg in sys.argv[1:]:
     skipped.add(arg)
