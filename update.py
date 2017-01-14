87e92c141af0b776807adc4037f031e311b555a1
Jon Eyolfson <jon@eyl.io>
2017-01-14 14:57:09 -0500

    emacs-powerline added to skip

--- update.py
+++ update.py
@@ -18,7 +18,7 @@ import os
 import sys
 from subprocess import call
 
-skipped = {'sabnzbd', 'python2-pygments-style-solarized', 'eyl-launcher', 'teensy-tools'}
+skipped = {'sabnzbd', 'python2-pygments-style-solarized', 'eyl-launcher', 'teensy-tools', 'emacs-powerline'}
 
 vcs_package = {'sickbeard', 'oh-my-zsh', 'emacs-rust-mode', 'packer', 'firefox-pentadactyl', 'emacs-color-theme-solarized', 'prezto'}
 
