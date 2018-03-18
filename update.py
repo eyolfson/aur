2c6f3d72ffb0f356d9bb8c3ba79c1f3ca6752884
Jon Eyolfson <jon@eyl.io>
2018-03-17 23:41:43 -0400

    Copyright year

--- update.py
+++ update.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright 2014 Jon Eyolfson
+# Copyright 2014-2018 Jon Eyolfson
 #
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
